31f04c7781a552ca2e96a35ee4ff74c29799e6bf
Kohwang <soongsil.ai.24@gmail.com>
2024-11-14 18:20:25 +0900

    Add files via upload

---
+++ law_chatbot/streamlit_llm.py
@@ -0,0 +1,48 @@
+# streamlit run streamlit_llm.py
+
+import streamlit as st
+import random
+import time
+
+# 외부 접속을 위한 임시 URL 생성
+external_url = st.experimental_tunnel("0.0.0.0", 8501)  # 기본 포트는 8501
+st.write(f"외부에서 접근하려면 다음 링크를 사용하세요: {external_url}")
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
